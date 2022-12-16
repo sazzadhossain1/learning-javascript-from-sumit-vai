b55841a78c83590db04c84570f55036bed832fa6
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-12-16 14:24:54 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,19 @@
+function sleep(name, time) {
+  console.log(`${name} is sleeping from ${time} PM`);
+  return 4;
+}
+sleep("Sazzad", 10);
+sleep("Shayla", 12);
+sleep("Yeasin", 1);
+console.log(sleep("ky", 39));
+
+//////////////////////////////////////////////
+
+function myFunction(a, b) {
+  return a * b;
+}
+
+const x = myFunction(2, 3);
+console.log(x);
+
+// <!-- 12 number ta ses -->
