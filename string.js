8eefc46972c0b90ec46d13ec2a891d284592100f
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-12-16 16:22:11 +0600

    string method

---
+++ string.js
@@ -0,0 +1,29 @@
+let text = "dfsfsdfsadfsdfsadfsdfsadf";
+console.log(text);
+console.log(text.length);
+console.log(new Set(text));
+
+////////////////////////////////////
+
+const str = "I love something";
+const replaceMethod = str.replace("something", "Javascript");
+console.log(replaceMethod);
+
+let text1 = "Hello";
+let text2 = "World";
+
+console.log(text1.concat(" ", text2));
+
+const text3 = "Hello Bangladesh";
+console.log(text3.charAt(500));
+console.log(text3[500]);
+
+const spliteMethod1 = text3.split();
+const spliteMethod2 = text3.split(",");
+const spliteMethod3 = text3.split(" ");
+const spliteMethod4 = text3.split("");
+console.log(spliteMethod1);
+console.log(spliteMethod2);
+console.log(spliteMethod3);
+console.log(spliteMethod4);
+// <!-- 16 number ta ses -->
